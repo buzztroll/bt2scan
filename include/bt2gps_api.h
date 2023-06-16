a41068d9ae8f5f095c7d876a16f24e1971679b09
John Bresnahan <buzztroll@gmail.com>
2023-06-16 15:29:59 -0500

    Refactored GPS module

--- include/bt2gps_api.h
+++ include/bt2gps_api.h
@@ -5,6 +5,8 @@
 #include <termios.h>
 #include <unistd.h>
 #include <errno.h>
+#include <pthread.h>
+#include <time.h>
 
 #define BT2_GPS_SUCCESS 0
 #define BT2_GPS_ERROR -1
@@ -13,6 +15,18 @@
 
 typedef struct bt2_gps_handle_s {
     int serial_port;
+    pthread_cond_t cond;
+    pthread_mutex_t mutex;
+    pthread_t thread_id;
+
+    float last_known_lat;
+    float last_known_lon;
+    time_t update_time;
+
+    int running;
+
+    int error_interval;
+    int interval_time;
 } bt2_gps_handle_t;
 
 int bt2_gps_init(
@@ -20,8 +34,16 @@ int bt2_gps_init(
 
 int bt2_gps_destroy(bt2_gps_handle_t * handle);
 
-int bt2_gps_get_location(
+int bt2_gps_get_location_now(
     bt2_gps_handle_t * gps_handle, float * out_lat, float * out_lon);
+
+
+int bt2_gps_start(bt2_gps_handle_t * gps_handle, int interval_time, int error_interval);
+
+int bt2_gps_stop(bt2_gps_handle_t * gps_handle);
+
+int bt2_gps_get_last_known_location(
+    bt2_gps_handle_t * gps_handle, float * out_lat, float * out_lon, time_t * when);
 
 
 #endif
